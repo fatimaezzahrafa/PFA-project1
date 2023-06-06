f6b7e059037c7b0ca286f11a0c1f9cff9c829b79
fatimaezzahrafa <118165347+fatimaezzahrafa@users.noreply.github.com>
2023-06-06 22:35:48 +0100

    Add files via upload

---
+++ userdash/dash2.js
@@ -0,0 +1,48 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+    },
+    {
+        productName:'LARVENDER KF102 Drone',
+        productName:'36378',
+        paymentStatus: 'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'Ruko F11 Pro Drone',
+        productName:'49347',
+        paymentStatus:'Due',
+        shipping:'pending'
+    },
+    {
+        productName:'Drone with camera drone',
+        productName:'96996',
+        paymentStatus:'paid',
+        shipping:'Delivered'
+
+    },
+    {
+        productName:'Gps 4k Drone',
+        productName:'22821',
+        paymentStatus:'paid',
+        shipping:'Delivered'
+
+    },
+    {
+        productName:'DJI Air 25',
+        productName:'81475',
+        paymentStatus:'Due',
+        shipping:'pending'
+
+    },
+    {
+        productName:'Lozenge Drone',
+        productName:'00482',
+        paymentStatus:'paid',
+        shipping:'Delivered'
+    },
+
+]
